72bbe81d30efa12b4651d6a6ca76d3b7877f1635
HarlanovichAleksey <159518586+HarlanovichAleksey@users.noreply.github.com>
2025-03-21 15:14:52 +0300

    Add files via upload

---
+++ Desktop app/Desktop app/MDIMainForm.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Desktop_app
+{
+    public partial class MDIMainForm : Form
+    {
+        public MDIMainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void открытьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // Создаем экземпляр формы калькулятора
+            CalculatorForm calculatorForm = new CalculatorForm();
+
+            // Указываем, что это дочернее окно MDI
+            calculatorForm.MdiParent = this;
+
+            // Показываем форму
+            calculatorForm.Show();
+        }
+
+        private void закрытьПоследнееОкноToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // Закрываем последнее открытое дочернее окно
+            foreach (Form childForm in this.MdiChildren)
+            {
+                childForm.Close();
+                break;
+            }
+        }
+
+        private void выходToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
---
+++ Desktop app/Desktop app/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанных со сборкой.
+[assembly: AssemblyTitle("Desktop app")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Desktop app")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, следует установить атрибут ComVisible в TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("27b29905-b548-4fc4-b073-5e19d09b94c9")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
